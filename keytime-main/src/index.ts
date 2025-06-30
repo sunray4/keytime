4f5b11cf0e1b88b98eb4e449045f4ec0fe8c24c6
sunray4 <sunray4@users.noreply.github.com>
2025-06-29 18:14:10 -0700

    create web socket server

--- keytime-main/src/index.ts
+++ keytime-main/src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import { server } from "./server";
 import { reposCommand } from "./commands/reposCommand";
 const program = new Command();
 
@@ -9,6 +10,9 @@ program
 .description("Keytime is a tool to help you manage your time")
 .version("1.0.0");
 
+server();
+
 reposCommand(program);
 
 program.parse();
+
---
+++ keytime-main/src/server.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from 'express';
+import WebSocket, { WebSocketServer } from "ws";
+
+const app = express();
+
+app.use(express.json());
+
+const wss = new WebSocketServer({port: 8081});
+// change ws to use the same port as the express server for production
+// const server = app.listen(8080);
+// const wss = new WebSocketServer({ 
+//   server,           // Use same HTTP server
+//   path: '/ws'       // Different path instead of port
+// });
+
+export function server(): void {
+  try {
+    app.listen(8080, () => console.log('Server started on port 8080'));   
+
+    wss.on('connection', (ws) => {
+        ws.on('error', (error) => {
+            console.error('WebSocket error:', error);
+        });
+        
+        // debugging code
+        console.log('client connected');
+        ws.send('Hello from server');
+
+        ws.on('message', (data) => {
+            console.log('Received message:', data);
+        });
+
+    wss.on('close', () => console.log('Connection closed'));
+    });
+
+ 
+
+
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
